4c71dfcab329509ca0de0e4fec82115754cf2d5d
leekirito <leekirito363@gmail.com>
2024-11-25 09:23:38 +0800

    new

---
+++ PlatformAdmin.py
@@ -0,0 +1,13 @@
+class PlatformAdmin():
+    def __init__(self):
+        self.username  = "admin"
+        self.password = "admin"
+
+    def remove_student():
+        pass
+
+    def remove_instructor():
+        pass
+
+    def remove_course():
+        pass
